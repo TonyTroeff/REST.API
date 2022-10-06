15dba5ca68f2cfdeed3571b747fef608bfa8d6f9
Tony Troeff <tonyslav_troeff@outlook.com>
2022-10-06 08:36:54 +0300

    Added a custom attribute we can use to simplify the content negotiation process.

--- API.Models/HATEOAS/HateoasLink.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace API.Models.HATEOAS;
-
-public class HateoasLink
-{
-    public string Url { get; set; }
-    public string Rel { get; set; }
-    public string Method { get; set; }
-}
---
+++ API/ContentNegotiation/WithVendorSupportAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace API.ContentNegotiation;
+
+using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class WithVendorSupportAttribute : ProducesAttribute
+{
+    public WithVendorSupportAttribute(params string[] supportedVendorMediaTypes)
+        : base (MediaTypeNames.Application.Json)
+    {
+        foreach (var t in supportedVendorMediaTypes)
+        {
+            this.ContentTypes.Add( VendorMediaTypes.WithSuffix(VendorMediaTypes.WithHateoas(t), "json"));
+            this.ContentTypes.Add(VendorMediaTypes.WithSuffix(t, "json"));
+        }
+    }
+}
